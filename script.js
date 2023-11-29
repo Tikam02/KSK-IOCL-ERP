3c49c65588efd5876cd221e4c8a10b0851e45ec3
Tikam Singh Alma <timonalma81@gmail.com>
2023-11-29 23:58:36 +0530

    Update script

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
-/* script.js */
-document.addEventListener('DOMContentLoaded', function () {
-    var form = document.getElementById('bootstrapForm');
-
-    form.addEventListener('submit', function (event) {
-        event.preventDefault();
-
-        var formData = new FormData(form);
-
-        fetch('https://docs.google.com/forms/d/e/1FAIpQLSfBvRMPRejFulKg4pQh3TSdbh1rfg13FA8WUYOzA8oBC-aL4w/formResponse', {
-            method: 'POST',
-            body: formData
-        })
-        .then(function (response) {
-            if (response.ok) {
-                alert('Form Submitted. Thanks.');
-            } else {
-                alert('Error submitting form.');
-            }
-        })
-        .catch(function (error) {
-            console.error('Error:', error);
-        });
-    });
-});
+// This script requires jQuery and jquery-form plugin
+// You can use these ones from Cloudflare CDN:
+// <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+// <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js" integrity="sha256-2Pjr1OlpZMY6qesJM68t2v39t+lMLvxwpa8QlRjJroA=" crossorigin="anonymous"></script>
+//
+$('#bootstrapForm').submit(function (event) {
+    event.preventDefault()
+    var extraData = {}
+    $('#bootstrapForm').ajaxSubmit({
+        data: extraData,
+        dataType: 'jsonp',  // This won't really work. It's just to use a GET instead of a POST to allow cookies from different domain.
+        error: function () {
+            // Submit of form should be successful but JSONP callback will fail because Google Forms
+            // does not support it, so this is handled as a failure.
+            alert('Form Submitted. Thanks.')
+            // You can also redirect the user to a custom thank-you page:
+            // window.location = 'http://www.mydomain.com/thankyoupage.html'
+        }
+    })
+})
